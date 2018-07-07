882b9a33c82e9e45a2d58fa79813bb3d11318692
MUTO <tskyotosu@gamil.com>
2018-07-08 07:27:17 +0900

    create ForestController.java

---
+++ forest/ForestController.java
@@ -0,0 +1,22 @@
+package forest;
+
+import mvc.Controller;
+
+import java.awt.event.MouseEvent;
+
+public class ForestController extends Controller{
+
+    public ForestController(){
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+        super.mouseClicked(mouseEvent);
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent mouseEvent) {
+        super.mouseDragged(mouseEvent);
+    }
+}
