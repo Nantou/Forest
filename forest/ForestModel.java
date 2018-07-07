40c6e5f4c968bad5a60c9eb5927738bdb4e4aa0b
MUTO <tskyotosu@gamil.com>
2018-07-08 07:27:43 +0900

    create ForestModel.java

---
+++ forest/ForestModel.java
@@ -0,0 +1,21 @@
+package forest;
+
+import mvc.Model;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+
+public class ForestModel extends Model {
+
+    private Forest aForest;
+
+    public Forest getForest() {
+        return aForest;
+    }
+
+    public void mouseClicked(Point aPoint, MouseEvent aMouseEvent){
+
+    }
+
+}
+
