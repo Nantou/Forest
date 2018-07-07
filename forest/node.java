55f3d9c21d393b64723033bc539900f120f737c2
Ushio Hiroto <g1644132.cac@gmail.com>
2018-07-07 19:56:14 +0900

    クラス図通りにクラスを設置

--- forest/node.java
+++ forest/node.java
@@ -4,9 +4,48 @@ import java.awt.Dimension;
 import java.awt.Point;
 import java.io.File;
 import javax.swing.JFrame;
+import java.util.ArrayList;
 
-public static main(String[] arguments){
+public class Node{
+  private String nodeName;
+  private int nodeNumber;
+  private boolean visit;
+  private Node parent;
+  private ArrayList<Node> children;
+}
 
+public Node(int number,String name){
 
-  return;
+}
+
+public String getName(){
+
+}
+
+public int getNumber(){
+
+}
+
+public Node getParent(){
+
+}
+
+public ArrayList<Node> getChildren(){
+
+}
+
+public boolean getVisit(){
+
+}
+
+public void setParent(Node aNode){
+
+}
+
+public void setChildren(Node aNode){
+
+}
+
+public void setVisit(boolean flag){
+
 }
