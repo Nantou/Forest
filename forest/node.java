ff2a19aad957f7f0734114061a035f35ffc6c175
Ushio Hiroto <g1644132.cac@gmail.com>
2018-07-26 17:08:38 +0900

    Node完成しました

--- forest/node.java
+++ forest/node.java
@@ -15,37 +15,44 @@ public class Node{
 }
 
 public Node(int number,String name){
-
+  this.nodeName = name;
+  this.nodeNumber = number;
+  this.parent = null;
+  this.children = new ArrayList<Node>();
+  this.visit = false;
 }
 
 public String getName(){
-
+  return this.nodeName;
 }
 
 public int getNumber(){
-
+  return this.nodeNumber;
 }
 
 public Node getParent(){
-
+  return this.parent;
 }
 
 public ArrayList<Node> getChildren(){
-
+  return this.parent;
 }
 
 public boolean getVisit(){
-
+  return this.visit;
 }
 
-public void setParent(Node aNode){
-
+public void setParent(Node parent){
+  this.parent = parent;
+  return;
 }
 
-public void setChildren(Node aNode){
-
+public void setChildren(Node children){
+  this.children.add(children);
+  return;
 }
 
 public void setVisit(boolean flag){
-
+  this.visit = flag;
+  return;
 }
