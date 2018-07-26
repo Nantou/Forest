d36db20855687551f90a08deb5b21a3ae0bd2535
MUTO <tskyotosu@gamil.com>
2018-07-27 04:45:38 +0900

    modify Node.java

--- forest/node.java
+++ forest/node.java
@@ -1,58 +1,56 @@
 package forest;
 
-import java.awt.Dimension;
-import java.awt.Point;
-import java.io.File;
-import javax.swing.JFrame;
+import javax.swing.JLabel;
 import java.util.ArrayList;
 
-public class Node{
-  private String nodeName;
-  private int nodeNumber;
-  private boolean visit;
-  private Node parent;
-  private ArrayList<Node> children;
-}
+public class Node extends JLabel {
+    private String nodeName;
+    private int nodeNumber;
+    private boolean visit;
+    private Node parent;
+    private ArrayList<Node> children;
 
-public Node(int number,String name){
-  this.nodeName = name;
-  this.nodeNumber = number;
-  this.parent = null;
-  this.children = new ArrayList<Node>();
-  this.visit = false;
-}
+    public Node(int number, String name) {
+        this.nodeName = name;
+        this.nodeNumber = number;
+        this.parent = null;
+        this.children = new ArrayList<Node>();
+        this.visit = false;
+    }
 
-public String getName(){
-  return this.nodeName;
-}
+    public String getName() {
+        return this.nodeName;
+    }
 
-public int getNumber(){
-  return this.nodeNumber;
-}
+    public int getNumber() {
+        return this.nodeNumber;
+    }
 
-public Node getParent(){
-  return this.parent;
-}
+    public Node getParent() {
+        return this.parent;
+    }
 
-public ArrayList<Node> getChildren(){
-  return this.parent;
-}
+    public ArrayList<Node> getChildren() {
+        return this.children;
+    }
 
-public boolean getVisit(){
-  return this.visit;
-}
+    public boolean getVisit() {
+        return this.visit;
+    }
 
-public void setParent(Node parent){
-  this.parent = parent;
-  return;
-}
+    public void setParent(Node parent) {
+        this.parent = parent;
+        return;
+    }
 
-public void setChildren(Node children){
-  this.children.add(children);
-  return;
-}
+    public void setChildren(Node children) {
+        this.children.add(children);
+        return;
+    }
 
-public void setVisit(boolean flag){
-  this.visit = flag;
-  return;
+    public void setVisit(boolean flag) {
+        this.visit = flag;
+        return;
+    }
+
 }
