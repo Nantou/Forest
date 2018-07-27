9062c93cc6a7f528709ed7b6a85d862727561cc0
makoto <horizon.jud.p01s@mail.com>
2018-07-27 10:46:30 +0900

    ForestView

--- forest/ForestView.java
+++ forest/ForestView.java
@@ -1,21 +1,55 @@
 package forest;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import mvc.View;
 
-import java.awt.*;
+public class ForestView extends View
+{
+	public ForestView(ForestModel aModel, ForestController aController)
+	{
+		super(aModel, aController);
+    this.model = aModel;
+    this.setSize(1500,1500);
+	}
 
-public class ForestView extends View {
+  public void growTree(Graphics aGraphics)
+  {
+		int width = this.getWidth();
+		int height = this.getHeight();
+		aGraphics.setColor(Color.white);
+    aGraphics.fillRect(0, 0, width, height);
 
-    private ForestModel model;
-    private ForestController controller;
+		BufferedImage picture = (BufferedImage)this.createImage(width, height);
+    Graphics aGraphicsBuffer = picture.getGraphics();
+    aGraphicsBuffer.setColor(Color.white);
+    aGraphicsBuffer.fillRect(0, 0, width, height);
+    aGraphicsBuffer.setColor(Color.black);
 
-    public ForestView(ForestModel aModel,ForestController controller){
-        super(aModel,controller);
-    }
+		ForestModel aForestModel = (ForestModel)(this.model);
+    ArrayList<Branch> branches = aForestModel.getForest().getBranches();
+    for(Branch aBranch : branches)
+		{
+			Point parentPoint = aBranch.getParentNode().getLocation();
+      Point childPoint = aBranch.getChildNode().getLocation();
+      Dimension parentDimension = aBranch.getParentNode().getSize();
+      Dimension childDimension = aBranch.getChildNode().getSize();
+			aGraphicsBuffer.drawLine(parentPoint.x + parentDimension.width,parentPoint.y + parentDimension.height/2,childPoint.x,childPoint.y + childDimension.height/2);
+		}
 
-    public void growTree(Graphics aGrapics){
+		HashMap<Integer,Node> nodes = aForestModel.getForest().getNodes();
+		for (Node aNode : nodes.values())
+		{
+			Point aPoint = aNode.getLocation();
+      Dimension aDimension = aNode.getSize();
+      aGraphicsBuffer.setFont(aNode.getFont());
+      aGraphicsBuffer.drawString(aNode.getName(), aPoint.x + Const.RECT_LINE_THICK, aPoint.y + aNode.getSize().height - Const.RECT_LINE_THICK * 2 - 1); //1pxがずれる理由がまったくわかりませんでした。
+      aGraphicsBuffer.drawRect(aPoint.x, aPoint.y, aDimension.width, aDimension.height);
+		}
 
-    }
-
-
+		Point aPoint = this.scrollAmount();
+		aGraphics.drawImage(picture, 0-aPoint.x, 0-aPoint.y, this);
+		return;
+	}
 }
